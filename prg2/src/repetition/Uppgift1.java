117c6fd127ae15d369f759fc60e9550bc5645dc0
Micho421 <michelelias333@hotmail.com>
2019-09-13 13:30:00 +0200

    Create Uppgift1.java

---
+++ prg2/src/repetition/Uppgift1.java
@@ -0,0 +1,22 @@
+package repetition;
+
+public class Uppgift1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int summa = 0;
+		int x = 0;
+		
+		while(x < 50) {
+			
+			summa += x;
+			x++;
+			
+		
+		}
+		
+		System.out.println(summa);
+	}
+
+}
