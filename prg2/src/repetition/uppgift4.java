3ced26b2309093afe31b736f2a6894cba2cf1a70
Micho421 <michelelias333@hotmail.com>
2019-09-16 08:46:32 +0200

    Create uppgift4.java

---
+++ prg2/src/repetition/uppgift4.java
@@ -0,0 +1,18 @@
+package repetition;
+
+import java.util.ArrayList;
+
+public class uppgift4 {
+
+	private static ArrayList<Object> Cristiano = new ArrayList<Object>();
+	
+	public static void add (Object a) {
+		Cristiano.add(a);
+	}
+
+	public static ArrayList<Object> returnera(){
+		return Cristiano;
+		
+	}
+	
+}
